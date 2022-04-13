aede22a5f8ea436d4a916a58b912f407a4665734
Helmut Kemper <helmut.kemper@gmail.com>
2022-04-13 19:23:02 -0300

    working

--- global.go
+++ global.go
@@ -2,11 +2,11 @@ package global
 
 import (
 	coordinateManager "github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.coordinate"
-	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/canvas"
-	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/document"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/factoryBrowserDocument"
-	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/factoryBrowserStage"
+	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/globalDocument"
+	//"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/factoryBrowserStage"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/html"
+	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.webbrowser/javascript/canvas"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/engine"
 )
 
@@ -16,7 +16,7 @@ type Config struct {
 	Stage          *canvas.Stage
 	Engine         *engine.Engine
 	Html           *html.Html
-	Document       document.Document
+	Document       globalDocument.Document
 	Density        float64
 	DensityManager *coordinateManager.Density
 }
@@ -29,15 +29,15 @@ func init() {
 	Global.Engine = &engine.Engine{}
 	Global.Density = 1.0
 	Global.DensityManager = &coordinateManager.Density{}
-	Global.Stage = factoryBrowserStage.NewStage(
-		Global.Html,
-		Global.Engine,
-		Global.Document,
-		"stage-auto-build",
-		Global.Density,
-		Global.DensityManager,
-	)
+	//Global.Stage = factoryBrowserStage.NewStage(
+	//	Global.Html,
+	//	Global.Engine,
+	//	Global.Document,
+	//	"stage-auto-build",
+	//	Global.Density,
+	//	Global.DensityManager,
+	//)
 
-	Global.Canvas = &Global.Stage.Canvas
-	Global.ScratchPad = &Global.Stage.ScratchPad
+	//Global.Canvas = &Global.Stage.Canvas
+	//Global.ScratchPad = &Global.Stage.ScratchPad
 }
